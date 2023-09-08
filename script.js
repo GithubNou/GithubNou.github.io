63998d847d33a9b9934e9b9ac05e2baf53d2053e
GithubNou <144323370+GithubNou@users.noreply.github.com>
2023-09-08 12:19:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 // define your images here
 var images = [
- "https://github.com/GithubNou/website/blob/main/cedre_ultralowres_samifarra-2.jpg?raw=true",
+  "https://github.com/GithubNou/website/blob/main/cedre_ultralowres_samifarra-2.jpg?raw=true",
   "https://github.com/GithubNou/website/blob/main/cedre_ultralowres_samifarra-17.jpg?raw=true",
   "https://github.com/GithubNou/website/blob/main/cedre_ultralowres_samifarra-8.jpg?raw=true"
 ];
 
-// this chooses a random number from all available image indices
-var randomImage = images[Math.floor(Math.random() * images.length)];
-console.log(randomImage);
-// make the URL into a proper image tag
+// initialize the index of the current image
+var currentIndex = Math.floor(Math.random() * images.length);
+
+// function to change the image to the next one
+function changeImage() {
+  // increment the index
+  currentIndex++;
+  
+  // if the index exceeds the length of the images array, reset it to 0
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  
+  // get the next image URL based on the current index
+  var nextImage = images[currentIndex];
+  
+  // make the URL into a proper image tag
+  var image = "<img src='" + nextImage + "'>";
+  
+  // update the image in the div
+  document.getElementById("img-load").innerHTML = image;
+}
+
+// add event listener to the image element
+document.getElementById("img-load").addEventListener("click", changeImage);
+
+// display the initial random image
+var randomImage = images[currentIndex];
 var image = "<img src='" + randomImage + "'>";
-// append to the div
 document.getElementById("img-load").innerHTML = image;
 
 var encEmail = "ZW1haWxAYWRkcmVzcy5jb20=";
